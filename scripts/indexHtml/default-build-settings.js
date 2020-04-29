7d44ba97902cb60bd7bb7169ca1589251a2c89f3
Julien Renaux <julien.renaux@forgerock.com>
2020-04-29 09:46:27 +0200

    adding viewport meta

--- scripts/indexHtml/default-build-settings.js
+++ scripts/indexHtml/default-build-settings.js
@@ -16,6 +16,11 @@ module.exports = {
         tag: "<title>Forgerock App</title>",
         order: 3,
       },
+      {
+        id: "viewport",
+        tag:
+          '<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />',
+      },
     ],
     body: {},
   },
